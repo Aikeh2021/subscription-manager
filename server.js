3aa392572c7de4255d02af87369ff4c9a7cd0953
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-02 18:14:42 -0500

    Set up the function to connect the server to mongoose. Set up two functions to tell us if the connection was successful or to console.log the error if one occurs.

--- server.js
+++ server.js
@@ -8,6 +8,26 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/subscription-manager",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  );
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected!");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+
 app.get("/api/config", (req, res) => {
     res.json(
         {
