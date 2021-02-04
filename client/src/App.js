6f9c77b674184be1d1f95fd74d2e4a1e0814a21d
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-04 15:02:37 -0500

    Added a monthly plan for basci, standard and premium plans in the subscriptions.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./containers/Home/Home";
+import LoginPage from "./containers/LoginPage/LoginPage";
 import SignUpForm from "./containers/SignUpForm/SignUpForm";
 import Subscriptions from "./containers/Subscriptions/Subscriptions";
 import UserDashboard from "./containers/UserDashboard/UserDashboard";
@@ -13,6 +14,7 @@ function App() {
       <Navbar/>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/loginpage" component={LoginPage} />
         <Route exact path="/signup" component={SignUpForm} />
         <Route exact path="/dashboard" component={UserDashboard} />
         <Route exact path="/subscriptions" component={Subscriptions} />
