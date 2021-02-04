86528e07c59a7f17bf9fc919b928bcea2288ebcc
Bhargava Patel <bhargavapatel82@gmail.com>
2021-02-04 13:37:49 -0500

    accepted changes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,19 @@
-<<<<<<< HEAD
-import { useEffect } from "react";
-import axios from "axios";
-
-function App() {
-  useEffect(() => {
-    axios.get("/api/config").then((response) => {
-      console.log(response.data);
-    });
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./containers/Home/Home";
 import SignUpForm from "./containers/SignUpForm/SignUpForm";
 
-
-
-
 function App() {
   return (
     <Router>
-      <Navbar/>
-      <SignUpForm/>
-    
+      <Navbar />
+      <SignUpForm />
+
       <Switch>
         <Route exact path="/" component={Home}></Route>
       </Switch>
     </Router>
->>>>>>> b397be2272922bfadacc417b5ffe2ada30ddc506
   );
 }
 
 export default App;
-
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,21 +5,21 @@ const navbar = () => {
     return (
         <div>
            {/* <!-- Dropdown Structure --> */}
-<ul id="dropdown1" class="dropdown-content">
-  <li><a href="#!">one</a></li>
-  <li><a href="#!">two</a></li>
+<ul id="dropdown1" className="dropdown-content">
+  <li><a href="/">one</a></li>
+  <li><a href="/">two</a></li>
   <li class="divider"></li>
-  <li><a href="#!">signout</a></li>
+  <li><a href="/">signout</a></li>
 </ul>
 <nav>
-  <div class="nav-wrapper">
-    <a href="#!" class="brand-logo">Subscriptions</a>
-    <ul class="right hide-on-med-and-down">
-      <li><a href="#">login</a></li>
-      <li><a href="#">signup</a></li>
+  <div className="nav-wrapper">
+    <a href="/" className="brand-logo">Subscriptions</a>
+    <ul className="right hide-on-med-and-down">
+      <li><a href="/">login</a></li>
+      <li><a href="/">signup</a></li>
       
  {/* This is a drop down arroww */}
-      {/* <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i class="material-icons right">arrow_drop_down</i></a></li> */}
+      {/* <li><a className="dropdown-trigger" href="#!" data-target="dropdown1">Dropdown<i className="material-icons right">arrow_drop_down</i></a></li> */}
     </ul>
   </div>
 </nav>
