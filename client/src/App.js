0cd041c72d74515adc810dd5cb81317282b5b7b5
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-03 21:44:37 -0500

    Added the link for materialize icons to the index.html. Scaffolded out the basic dashboard page table.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,7 @@
-import { useEffect } from 'react';
-import axios from 'axios';
 import UserDashboard from './components/UserDashboard/UserDashboard';
 
 function App() {
-  useEffect(() => {
-    axios.get("/api/config").then((response) => {
-      console.log(response.data);
-    })
-  }, [])
+
   return (
     <UserDashboard />
   );
--- client/src/components/UserDashboard/UserDashboard.jsx
+++ client/src/components/UserDashboard/UserDashboard.jsx
@@ -1,12 +1,37 @@
-
-import React from 'react';
+import React from "react";
 
 const UserDashboard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+    <div class="container">
+    <div class="row">
+    <div class="col s12">
+    <table>
+        <thead>
+          <tr>
+            <th>Subscriptions</th>
+            <th>Current Amount</th>
+            <th>Subscription Price Change</th>
+            <th>Update Subscription</th>
+            <th>Delete Subscription</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Image</td>
+            <td>$13.99</td>
+            <td>$1.00</td>
+            <td><a class="waves-effect waves-light btn-small"><i class="material-icons left">delete</i>DELETE</a></td>
+            <td><a class="waves-effect waves-light btn-small"><i class="material-icons left">update</i>UPDATE</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    </div>
+    </div>
+    </div>
+  );
 };
 
 export default UserDashboard;
