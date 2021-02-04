be324a03f15e78440e17e53c96898304826fb8d3
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-04 15:29:56 -0500

    updated the signup form.Added a button in the subscription page.

--- client/src/containers/Subscriptions/Subscriptions.jsx
+++ client/src/containers/Subscriptions/Subscriptions.jsx
@@ -2,27 +2,84 @@ import React from 'react';
 
 const Subscriptions = () => {
     return (
-        <div>
+      <div>
+<div className="container">
+        <h5> Please select a Subscription </h5>
+<div class="row">
+<label></label>
+<select class="browser-default" enabled>
+<option value="enabled" enabled value></option>
+<option value="1">Hulu</option>
+<option value="2">Netflix</option>
+<option value="3">Amazon</option>
+<option value="3">Apple music</option>
+</select>
+</div>
+</div>
+<div className="container">
+        <h5> Please select a monthly plan </h5>
+<div class="row">
+<label></label>
+<select class="browser-default" enabled>
+<option value="enabled" enabled value>Basic</option>
+<option value="1">$5:99</option>
+<option value="2">$8.99</option>
+<option value="3">$12.99</option>
+
+</select>
+</div>
+</div>
 
 <div className="container">
-             <div class="row">
-             
-  <label>Browser Disabled</label>
-  <select class="browser-default" enabled>
-    <option value="enabled" enabled value>Choose your option</option>
-    <option value="1">Option 1</option>
-    <option value="2">Option 2</option>
-    <option value="3">Option 3</option>
-  </select>
+        <h5>  </h5>
+<div class="row">
+<label></label>
+<select class="browser-default" enabled>
+<option value="enabled" enabled value>Standard</option>
+<option value="1">$5:99</option>
+<option value="2">$8.99</option>
+<option value="3">$13.99</option>
+
+</select>
 </div>
 </div>
+
+
+<div className="container">
+        <h5> </h5>
+<div class="row">
+<label></label>
+<select class="browser-default" enabled>
+<option value="enabled" enabled value>Premium</option>
+<option value="1">$11.99</option>
+<option value="2">$17.99</option>
+<option value="3">$25.99</option>
+
+</select>
+
+
 </div>
-  
+
+<div>
+  <button className="btn waves-effect waves-light" type="submit" name="action">Add to dashboard
+   
+   </button>
+</div>
+
+</div>
+
+
+      </div>
+
+      
   );
 };
 
 export default Subscriptions;
+
+
+
+
   
-          
-  
+
   
