73ed4b143ef03b819e9624e67f8d0886f33a4f57
Bhargava Patel <bhargavapatel82@gmail.com>
2021-02-03 21:32:24 -0500

    deleted unwanted files and added homepage container.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect } from 'react';
-import axios from 'axios';
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
   useEffect(() => {
     axios.get("/api/config").then((response) => {
       console.log(response.data);
-    })
-  }, [])
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
