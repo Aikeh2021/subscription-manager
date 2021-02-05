17235ecdb424ae44253d06e93e5902474b0e9d63
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-05 11:15:14 -0500

    Decided to remove the small bit of content in the admin model. Will leave the scaffold empty until ready to work on it.

--- models/admin.js
+++ models/admin.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AdminSchema = new Schema ({
-    userName: {
-        type: String,
-        required: 'Please input a username'
-    }
+
 });
 
 const Admin = mongoose.model("Admin", AdminSchema);
