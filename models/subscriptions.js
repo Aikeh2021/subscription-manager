9fe6cf44bd9ac4add367a1e5a9c54a89fe0832d0
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-05 09:31:10 -0500

    Addressed an error on the schemas. Forgot to set them equal to new schemas using their variable names.

--- models/subscriptions.js
+++ models/subscriptions.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const Schema = mongoose.Schema({
+const SubscriptionSchema = new Schema ({
     subscription_name: {
         type: String,
         required: 'Choose a subscription'
