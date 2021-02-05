cf0ca1b20e190169713a6ab7db873336ede56971
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-05 09:37:27 -0500

    Added content to the user model.

--- models/users.js
+++ models/users.js
@@ -2,7 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-
+    first_name: {
+        type: String,
+        required: 'Please enter your first name'
+    },
+    last_name: {
+        type: String,
+        required: 'Please enter your last name'
+    },
+    email: {
+        type: String,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    password: {
+        type: String,
+        required: 'Password must be at least 5 characters'
+    },
+    subscriptions: {
+        type: Array
+    }
 });
 
 const User = mongoose.model("User", UserSchema);
