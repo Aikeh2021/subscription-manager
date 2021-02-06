7d1b33c7d68d7a8d4e7608956d4bae7dcd2652e1
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-06 10:23:35 -0500

    Removed the className validate becasue we're using a different validation.

--- client/src/containers/LoginPage/LoginPage.jsx
+++ client/src/containers/LoginPage/LoginPage.jsx
@@ -7,12 +7,12 @@ const LoginPage = () => {
     <form className="col s12">
       <div className="row">
         <div className="input-field col s12">
-          <input id="email" type="email" className="validate"/>
+          <input id="email" type="email"/>
           <label for="Email">Email</label>
       </div>
       <div classNameName="row">
         <div className="input-field col s12">
-          <input id="password" type="password" className="validate"/>
+          <input id="password" type="password"/>
           <label for="email">Password</label>
         </div>
       </div>
