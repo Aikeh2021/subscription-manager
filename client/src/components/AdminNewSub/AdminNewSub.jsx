222602a3d4282c022507542a277b4aa4583cc1a3
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-08 12:01:20 -0500

    Added some styling to center the new subscription form a little. Added a button at the bottom of the form.

--- client/src/components/AdminNewSub/AdminNewSub.jsx
+++ client/src/components/AdminNewSub/AdminNewSub.jsx
@@ -3,12 +3,14 @@ import React from "react";
 const NewSubscription = () => {
   return (
     <div>
-      <br />
-      <h1>Create A New Subscription</h1>
+
+      <div className="container">
+      <div className="divider"></div>
+      <h1 className="center-align">Create A New Subscription</h1>
       <div className="row">
-        <form className="col s9">
+        <form className="col s11 offset-m1">
           <div className="row">
-            <div className="input-field col s9">
+            <div className="input-field col s10">
               <input
                 placeholder="Enter new subscription name"
                 id="sub_name"
@@ -35,19 +37,27 @@ const NewSubscription = () => {
           </div>
           {/* Will need to edit this later to accept a url for when admin wants to add a thumbnail sized logo */}
           <div className="row">
-            <div className="input-field col s9">
+            <div className="input-field col s10">
               <input id="logo" type="text" />
               <label htmlFor="logo">Subscription Logo</label>
             </div>
           </div>
           <div className="row">
-            <div className="input-field col s9">
+            <div className="input-field col s10">
               <input id="category" type="text" />
               <label htmlFor="category">Service Type</label>
             </div>
           </div>
+          <div className="row">
+            <div className="col s9">
+            <a class="waves-effect waves-light btn-large"><i className="material-icons right">create</i>Add New Subscription</a>
+
+            </div>
+          </div>
         </form>
       </div>
+      </div>
+
     </div>
   );
 };
