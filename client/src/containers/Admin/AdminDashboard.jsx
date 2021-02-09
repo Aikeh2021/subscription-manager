e6b54549b33350850d3163ce18ad47cb7bd2bb81
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-09 12:57:29 -0500

    Added a heading tag on each of the dashboards to distinguish between the user and admin views. Added instructrions to the user's dashboard. Will add a way to personalize each user dashboard with their name to the header so they have another way to know they are accessing their personal account.

--- client/src/containers/Admin/AdminDashboard.jsx
+++ client/src/containers/Admin/AdminDashboard.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import Dashboard from '../../components/Dashboards/Dashboard';
 
 const AdminDashboard = () => {
+    const styles = {
+        h1: {
+            textAlign: "center"
+        }
+    }
     return (
         <div>
+            <h1 style={styles.h1}>Admin Subscription Dashboard</h1>
+            <br />
             <Dashboard />
         </div>
     );
--- client/src/containers/UserDashboard/UserDashboard.jsx
+++ client/src/containers/UserDashboard/UserDashboard.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import Dashboard from '../../components/Dashboards/Dashboard';
+import React from "react";
+import Dashboard from "../../components/Dashboards/Dashboard";
 
 const UserDashboard = () => {
+  const styles = {
+    h1: {
+      textAlign: "center",
+    },
+    p: {
+      textAlign: "center"
+    }
+  };
   return (
     <div>
+      <h1 style={styles.h1}>Subscription Dashboard</h1>
+      <div className="divider"></div>
+      <p style={styles.p}>
+        Welcome to your personalized subscription tracker! Update or delete
+        subscriptions as you please. Check your email for notifications when
+        your plan pricing changes.
+      </p>
       <Dashboard />
     </div>
   );
