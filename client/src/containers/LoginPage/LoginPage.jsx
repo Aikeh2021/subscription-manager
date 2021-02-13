3bc87b27e33670fdaa62b9a3a99bebce5bf87b30
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-13 09:53:58 -0500

    Updated the loginpage form.

--- client/src/containers/LoginPage/LoginPage.jsx
+++ client/src/containers/LoginPage/LoginPage.jsx
@@ -1,37 +1,39 @@
 import React , { useState}  from 'react';
 
-const LoginPage = () => {
+const LoginPage = (props) => {
 
   const [email, setEmail]= useState("");
   const [password, setPassword]= useState("");
 
     return (
+      <div>
+      <h1> </h1>
         <div className="container">
         <div className="row">
     <form className="col s12">
       <div className="row">
         <div className="input-field col s12">
-          <input id="email" type="email" value={email} onChange={(e) =>{
+          <input placeholder="" id="email" type="email" value={email} onChange={(e) =>{
                  setEmail(e.target.value);
          }}/>
           <label for="Email">Email</label>
       </div>
-      <div className="row">
+    
         <div className="input-field col s12">
-          <input id="password" type="password" value={password} onChange={(e) =>{
+
+          <input placeholder="" id="password" type="password" value={password} onChange={(e) =>{
                  setPassword(e.target.value);
          }}/>
           <label for="email">Password</label>
 
         </div>
       </div>
-      </div>
    </form> 
    <button className="btn waves-effect waves-light" type="submit" name="action">Login
    </button>
    </div>
-    </div>
-   
+   </div>
+   </div>
     );
 };
 
