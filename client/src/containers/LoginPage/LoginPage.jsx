fc56bc888aebbac8e187f56224394a8122382762
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-13 12:12:48 -0500

    updaed the loginForm with props and set the state.

--- client/src/containers/LoginPage/LoginPage.jsx
+++ client/src/containers/LoginPage/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const LoginPage = () => {
+const LoginPage = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -20,23 +20,24 @@ const LoginPage = () => {
                   setEmail(e.target.value);
                 }}
               />
-              <label htmlFor="Email">Email</label>
+              <label for="Email">{props.email}</label>
             </div>
-            <div className="row">
+           
               <div className="input-field col s12">
                 <input
                   id="password"
-                  type="password"
+                  type="password\"
                   value={password}
                   onChange={(e) => {
                     setPassword(e.target.value);
                   }}
                 />
-                <label htmlFor="email">Password</label>
+              <label for="Email">{props.password}</label>
               </div>
             </div>
-          </div>
+         
         </form>
+         </div>
         <button
           className="btn waves-effect waves-light"
           type="submit"
@@ -50,7 +51,6 @@ const LoginPage = () => {
           Login
         </button>
       </div>
-    </div>
     </>
   );
 };
