8a68792478bbee9e646f197411fb5fb4efd33e64
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-13 07:43:58 -0500

    Adding some styling to the pages to make the design a little more memorable. I think a good color scheme is purple, gray, white, green, and black. I wanted the site to have a sort of funky vibe. Added several sources to the credit section of the readme.

--- client/src/containers/LoginPage/LoginPage.jsx
+++ client/src/containers/LoginPage/LoginPage.jsx
@@ -1,38 +1,58 @@
-import React , { useState}  from 'react';
+import React, { useState } from "react";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [email, setEmail]= useState("");
-  const [password, setPassword]= useState("");
-
-    return (
-        <div className="container">
-        <div className="row">
-    <form className="col s12">
+  return (
+    <>
+    <h1 style={{textAlign: 'center', fontFamily: 'Monoton'}}>LOGIN TO YOUR ACCOUNT</h1>
+    <div className="container" style={{fontFamily: "Roboto"}}>
       <div className="row">
-        <div className="input-field col s12">
-          <input id="email" type="email" value={email} onChange={(e) =>{
-                 setEmail(e.target.value);
-         }}/>
-          <label for="Email">Email</label>
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <label for="Email">Email</label>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+                <label for="email">Password</label>
+              </div>
+            </div>
+          </div>
+        </form>
+        <button
+          className="btn waves-effect waves-light"
+          type="submit"
+          name="action"
+          style={{
+            borderRadius: 30,
+            backgroundColor: "#008000",
+            fontFamily: "Roboto",
+          }}
+        >
+          Login
+        </button>
       </div>
-      <div className="row">
-        <div className="input-field col s12">
-          <input id="password" type="password" value={password} onChange={(e) =>{
-                 setPassword(e.target.value);
-         }}/>
-          <label for="email">Password</label>
-
-        </div>
-      </div>
-      </div>
-   </form> 
-   <button className="btn waves-effect waves-light" type="submit" name="action">Login
-   </button>
-   </div>
     </div>
-   
-    );
+    </>
+  );
 };
 
 export default LoginPage;
--- client/src/containers/SignUpForm/SignUpForm.jsx
+++ client/src/containers/SignUpForm/SignUpForm.jsx
@@ -1,45 +1,59 @@
-import React from 'react';
-
+import React from "react";
 
 const SignUpForm = () => {
-    return (
-        <div>
-          <h1> </h1>
-          <div className="container">
-             <div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <input placeholder="First Name" id="first_name" type="text" class="validate"/>
-          <label for="first_name">First Name</label>
-        </div>
-        <div class="input-field col s6">
-          <input id="last_name" type="text" class="validate"/>
-          <label for="last_name">Last Name</label>
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", fontFamily: "Monoton" }}>
+        CREATE AN ACCOUNT
+      </h1>
+      <div className="container" style={{fontFamily: "Roboto"}}>
+        <div class="row">
+          <form class="col s12">
+            <div class="row">
+              <div class="input-field col s6">
+                <input
+                  placeholder="First Name"
+                  id="first_name"
+                  type="text"
+                  class="validate"
+                />
+                <label for="first_name">First Name</label>
+              </div>
+              <div class="input-field col s6">
+                <input id="last_name" type="text" class="validate" />
+                <label for="last_name">Last Name</label>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="email" type="email" class="validate" />
+                <label for="email">Email</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="password" type="password" class="validate" />
+                <label for="password">Password</label>
+              </div>
+            </div>
+            <button
+              class="btn waves-effect waves-light"
+              type="submit"
+              name="action"
+              style={{
+                borderRadius: 30,
+                backgroundColor: "#008000",
+                fontFamily: "Roboto",
+              }}
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
-    
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="email" type="email" class="validate"/>
-          <label for="email">Email</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate"/>
-          <label for="password">Password</label>
-        </div>
-      </div>
-      <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-   
-  </button>
-      
-    </form>
-  </div>
-        </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default SignUpForm;
