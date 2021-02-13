f7e91d19110d1c0d3ade816caf270ad19b1a229a
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-02-13 08:31:52 -0500

    Added the key value pare in the sate and added an onchange event for each input field.

--- client/src/containers/SignUpForm/SignUpForm.jsx
+++ client/src/containers/SignUpForm/SignUpForm.jsx
@@ -1,5 +1,9 @@
 import React, {useState} from 'react';
 
+const [firstName, setFirstName]= useState("");
+  const [lastName, setLastName]= useState("");
+  const [email, setEmail]= useState("");
+  const [password, setPassword]= useState("");
 
 const SignUpForm = (props) => {
     return (
@@ -10,28 +14,37 @@ const SignUpForm = (props) => {
     <form className="col s12">
       <div className="row">
         <div className="input-field col s6">
-          <input placeholder="First Name" id="first_name" type="text" className="validate"/>
+          <input placeholder="First Name" id="first_name" type="text" className="validate" value={firstName} onChange={(e) =>{
+                 setFirstName(e.target.value);
+         }}/>
           <label for="first_name">{props.first_name}</label>
         </div>
+
         <div className="input-field col s6">
-          <input id="last_name" type="text" className="validate"/>
+          <input id="last_name" type="text" className="validate" value={lastName} onChange={(e) =>{
+                 setLastName(e.target.value);
+         }}/>
           <label for="last_name">{props.last_name}</label>
         </div>
       </div>
     
       <div className="row">
         <div className="input-field col s12">
-          <input id="email" type="email" className="validate"/>
+          <input id="email" type="email" className="validate" value={email} onChange={(e) =>{
+                 setEmail(e.target.value);
+         }}/>
           <label for="email">{props.email}</label>
         </div>
       </div>
       <div className="row">
         <div className="input-field col s12">
-          <input id="password" type="password" className="validate"/>
+          <input id="password" type="password" className="validate" value={password} onChange={(e) =>{
+                 setPassword(e.target.value);
+         }}/>
           <label for="password">{props.password}</label>
         </div>
       </div>
-      <button className="btn waves-effect waves-light" type="submit" name="action">{props.submit}
+      <button className="btn waves-effect waves-light" type="submit" name="action">Add to Dashboard
    
   </button>
       
