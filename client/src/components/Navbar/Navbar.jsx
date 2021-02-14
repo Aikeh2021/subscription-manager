bbbcfad9aa78c65c3dcd3202f199d711b487c2f0
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-14 02:07:02 -0500

    Completed the dropdown menu on the navbar. Added some styling to the navlinks to make the font appear black against the menu's white background. For now the links all go to the homepage because the other pages have not been created yet.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,32 +1,41 @@
-// import { Dropdown } from 'materialize-css';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Link, NavLink } from 'react-router-dom';
-// import M from 'materialize-css';
-
-
-const navbar = () => {
+import M from "materialize-css";
 
 
 
+
+const Navbar = () => {
+
+useEffect(() => {
+  let elems = document.querySelectorAll('.dropdown-trigger');
+  let options = {
+    alignment: "right",
+        hover: true,
+        coverTrigger: false
+  };
+  M.Dropdown.init(elems, options);
+}, [])
+ 
 
     return (
         <div>
           {/* <!-- Dropdown Structure --> */}
 <ul id="dropdown1" className="dropdown-content">
-  <li><NavLink to="/">About</NavLink></li>
+  <li><NavLink to="/" style={{fontFamily: 'Roboto', color: 'Black'}}>About</NavLink></li>
   <li className="divider"></li>
-  <li><NavLink to="/">Contact Us</NavLink></li>
+  <li><NavLink to="/" style={{fontFamily: 'Roboto', color: 'Black'}}>Contact Us</NavLink></li>
   <li className="divider"></li>
-  <li><NavLink to="/">Sign Out</NavLink></li>
+  <li><NavLink to="/" style={{fontFamily: 'Roboto', color: 'Black'}}>Sign Out</NavLink></li>
 </ul>
 <nav style={{backgroundColor: "#4B0082", fontFamily: 'Roboto'}}>
   <div className="nav-wrapper">
     <Link to="/" style={{fontFamily: 'Monoton'}}>Sub Manny</Link>
-    <ul className="right hide-on-med-and-down" id="dropdown2">
+    <ul className="right hide-on-med-and-down">
       <li><NavLink to="/login">Login</NavLink></li>
       <li><NavLink to="/signup">Signup</NavLink></li>
       {/* <!-- Dropdown Trigger --> */}
-      <li><a className="dropdown-trigger" href="#!" data-target="dropdown2">Options<i className="material-icons right">arrow_drop_down</i></a></li>
+      <li><a className="dropdown-trigger" href="#!" data-target="dropdown1">Options<i className="material-icons right">arrow_drop_down</i></a></li>
     </ul>
   </div>
 </nav>
@@ -34,4 +43,4 @@ const navbar = () => {
     );
 };
 
-export default navbar;
+export default Navbar;
