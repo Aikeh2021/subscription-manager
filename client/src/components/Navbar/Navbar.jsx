7cafb19c1f8a2f211343e5750aa7c17fa1783bb7
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-14 04:37:31 -0500

    Started building out a sidenav. Created a folder and file for the sidenav but will not need it because I had to break up the sidenav to make it function the way I want (placed beside the logo on small screen sizes). Will Delete the folder and file later.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -15,7 +15,16 @@ useEffect(() => {
         coverTrigger: false
   };
   M.Dropdown.init(elems, options);
-}, [])
+}, []);
+
+useEffect(() => {
+let sidePiece = document.querySelectorAll('sidenav-trigger');
+let options2 = {
+  edge: 'right',
+
+};
+M.Dropdown.init(sidePiece, options2);
+}, []);
  
 
     return (
@@ -28,9 +37,11 @@ useEffect(() => {
   <li className="divider"></li>
   <li><NavLink to="/" style={{fontFamily: 'Roboto', color: 'Black'}}>Sign Out</NavLink></li>
 </ul>
+{/* Main Navbar */}
 <nav style={{backgroundColor: "#4B0082", fontFamily: 'Roboto'}}>
   <div className="nav-wrapper">
-    <Link to="/" style={{fontFamily: 'Monoton'}}>Sub Manny</Link>
+  <a href="/" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    <Link to="/" style={{fontFamily: 'Monoton', fontSize: 32}}>Sub Manny</Link>
     <ul className="right hide-on-med-and-down">
       <li><NavLink to="/login">Login</NavLink></li>
       <li><NavLink to="/signup">Signup</NavLink></li>
@@ -39,6 +50,22 @@ useEffect(() => {
     </ul>
   </div>
 </nav>
+{/* Side Nav Pieces */}
+<ul id="slide-out" class="sidenav">
+    <li><div class="user-view">
+      <div class="background">
+        <img src="images/office.jpg" />
+      </div>
+      <a href="#user"><img class="circle" src="images/yuna.jpg" /></a>
+      <a href="#name"><span class="white-text name">John Doe</span></a>
+      <a href="#email"><span class="white-text email">jdandturk@gmail.com</span></a>
+    </div></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+    <li><a href="#!">Second Link</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Subheader</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+  </ul>
         </div>
     );
 };
---
+++ client/src/components/Sidenav/Sidenav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidenav = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Sidenav;
