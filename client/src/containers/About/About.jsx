062aad4fddef69b45b6d30fbabc2c8a502890534
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-15 14:30:33 -0500

    Finished adding content to the about page.

--- client/src/containers/About/About.jsx
+++ client/src/containers/About/About.jsx
@@ -6,20 +6,19 @@ import SubMannyLogo from '../../assets/logos/SubMannyLogo.png';
 const About = () => {
     return (
         
-        <div className="center">
+        <div>
         <br />
-        <img src={SubMannyLogo} alt="Sub Manny Logo" style={{width: 300, height: 300, borderRadius: 30}}/>
+        <img src={SubMannyLogo} alt="Sub Manny Logo" className="valign center-block" style={{width: 300, height: 300, borderRadius: 30}}/>
         <h1 style={{textAlign:"center", fontFamily: 'Monoton'}}>ABOUT SUB MANNY</h1>
         <div className="divider"></div>
         <br />
         <div className="container">
             <div className="row">
             <div style={{paddingTop: 15, paddingBottom: 20, backgroundColor: "green", borderRadius: 30}}></div>
+            <AboutInfo />
             </div>
         </div>
         
-        
-        <AboutInfo />
     </div>
     );
 };
