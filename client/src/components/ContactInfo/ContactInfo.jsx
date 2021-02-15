8632915e23851a8b2494968524d6f5c4c9315ebe
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-15 17:14:49 -0500

    The Contact page content has been added.

--- client/src/components/ContactInfo/ContactInfo.jsx
+++ client/src/components/ContactInfo/ContactInfo.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
 
 const ContactInfo = () => {
-    return (
-        <div>
-           <p>This page needs content</p> 
-        </div>
-    );
+  return (
+    <div>
+      <h3 style={{ textAlign: "center" }}>Customer Support</h3>
+      <p style={{ fontSize: 20 }}>
+        Having issues with your account? No worries, just contact customer
+        support at the email address below:
+      </p>
+      <div className="center valign">
+        <a
+          style={{ textAlign: "center", fontSize: 30 }}
+          href="mailto:submannysupport@gmail.com"
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          submannysupport@gmail.com
+        </a>
+      </div>
+
+      <h3 style={{ textAlign: "center" }}>Contact The Development Team</h3>
+      <p style={{ fontSize: 20 }}>
+        If you would like to get in contact with the development team for Sub
+        Manny, please visit the github repository for this website to obtain the
+        contact information for the application contributors.
+      </p>
+      <div className="center valign">
+        <a
+          href="https://github.com/Aikeh2021/subscription-manager"
+          rel="noreferrer noopener"
+          target="_blank"
+          style={{ textAlign: "center", fontSize: 30 }}
+        >
+          Visit Sub Manny on Github
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default ContactInfo;
--- client/src/containers/Contact/Contact.jsx
+++ client/src/containers/Contact/Contact.jsx
@@ -4,20 +4,21 @@ import SubMannyLogo from '../../assets/logos/SubMannyLogo.png';
 
 const Contact = () => {
     return (
-        <div className="center">
+        <div>
             <br />
-            <img src={SubMannyLogo} alt="Sub Manny Logo" style={{width: 300, height: 300, borderRadius: 30}}/>
+            <img src={SubMannyLogo} alt="Sub Manny Logo" className="center valign center-block" style={{width: 400, height: 400, borderRadius: 30}}/>
             <h1 style={{textAlign:"center", fontFamily: 'Monoton'}}>CONTACT US</h1>
             <div className="divider"></div>
             <br />
             <div className="container">
                 <div className="row">
                 <div style={{paddingTop: 15, paddingBottom: 20, backgroundColor: "green", borderRadius: 30}}></div>
+                <ContactInfo />
+                <br />
+                <div style={{paddingTop: 15, paddingBottom: 20, backgroundColor: "green", borderRadius: 30}}></div>
                 </div>
             </div>
             
-            
-            <ContactInfo />
         </div>
     );
 };
