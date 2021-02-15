7c1ae9dc392795389ce08b7f5635f8bf0ba50e2a
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-14 20:49:20 -0500

    Changed the a tags to Links in the sidenav. Still unsure about the signout link. Thinking of maybe linking it to the home page and then alerting the user that they have signed out. Will run idea by team to see what they think. Will create a logo for the sidenav and replace the all caps text with the user's name when I make a route with a findBy method to retrieve the user's first name.

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -31,8 +31,8 @@ M.Sidenav.init(sidePiece, options2);
 
 
     return (
-        <div>
-          {/* <!-- Dropdown Structure --> */}
+<div>
+{/* <!-- Dropdown Structure --> */}
 <ul id="dropdown1" className="dropdown-content">
   <li><NavLink to="/" style={{fontFamily: 'Roboto', color: 'Black'}}>About</NavLink></li>
   <li className="divider"></li>
@@ -63,11 +63,13 @@ M.Sidenav.init(sidePiece, options2);
       <a href="#name"><span className="white-text name">John Doe</span></a>
       <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
     </div></li>
-    <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
-    <li><a href="#!">Second Link</a></li>
+    <li><Link to="/"><i className="material-icons">person</i>PUT USER'S NAME HERE</Link></li>
     <li><div className="divider"></div></li>
-    <li><a className="subheader">Subheader</a></li>
-    <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+    <li><Link to="/login">Login</Link></li>
+    <li><Link to="/signup">Signup</Link></li>
+    <li><Link to="/about">About</Link></li>
+    <li><Link to="/contact">Contact Us</Link></li>
+    <li><a className="waves-effect" href="#!">Sign Out</a></li>
   </ul>
         </div>
     );
