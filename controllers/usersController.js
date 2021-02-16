3a7f2fac07a9f690373dadd7c3f88b2a1c83f154
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-02-15 22:29:56 -0500

    I think I successfully populated the subscriptions for a single user. Still working on the post route for adding a subscription to a user's subscription array. Committing my changes to push the main branch up to Heroku.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const Subscription = require('../models/subscriptions');
 const router = express.Router();
 const User = require("../models/users");
+
 
 router.get("/:email", (req, res) => {
     //Getting a user by their email
@@ -32,5 +34,55 @@ router.post("/", (req, res) => {
         console.log(err)
     });
 });
+
+
+
+// app.post("/submit", ({body}, res) => {
+//     db.Book.create(body)
+//       .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+//       .then(dbLibrary => {
+//         res.json(dbLibrary);
+//       })
+//       .catch(err => {
+//         res.json(err);
+//       });
+//   });
+
+
+
+// router.post("/submitNew", ({body}, res) => {
+//     Subscription.create(body).then(({_id} => db.User.findOneAndUPdate({}, { $push: {subscriptions: _id} }, {new:true})))
+//     .then((usersNewSub) => {
+//         res.json(usersNewSub);
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+// })
+// router.post("/submitNew", ({body}, res) => {
+//     User.create(body).then(({_id}) => db.)
+// })
+
+router.get("/populated", (req, res) => {
+    User.find({}).populate("subscriptions")
+    //To return the subscriptions a single user is tracking
+    .then(dbUser => {
+        res.json(dbUser);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+router.post("/submit", ({body}, res) => {
+    Subscription.create(body)
+    .then(({_id}) => User.findOneAndUpdate({}, { $push: {subscriptions: _id} }, { new: true }))
+    .then(user => {
+        res.json(user);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+//To push a new subscription into a User's array of subscriptions
 
 module.exports = router;
